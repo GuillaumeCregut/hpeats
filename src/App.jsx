543a1790e53a38a2a3d869dd784051bdad6a85c0
belot nicolas <nicolas-belot_student2021@wilder.school>
2022-01-12 14:51:27 +0100

    add css

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,11 @@ import Home from './pages/Home';
 function App() {
   return (
     <div className="App">
-      <h1>Bienvenue à HPEats !</h1>
+      
       <Routes> 
-
         <Route exact path="/" element={<Home />} />
-       
       </Routes>
+      
       </div>
   );
 }
--- src/components/navBar/MenuFilters.jsx
+++ src/components/navBar/MenuFilters.jsx
@@ -1,29 +1,40 @@
-import React from 'react'
-import CardIcon from './CardIcon'
-import "./MenuFilters.css" 
+import React from 'react';
+import CardIcon from './CardIcon';
+import "./MenuFilters.css" ;
 
-import LogoVegan from '../../assets/img/LogoVegan.png'
-import Logoflexi from '../../assets/img/LogoFlexi.png'
-import LogoTradi from '../../assets/img/LogoTradi.png'
-import LogoPotion from '../../assets/img/LogoPotion.png'
-import LogoEtrange from '../../assets/img/LogoEtrange.png'
+import LogoVegan from '../../assets/img/LogoVegan.png';
+import Logoflexi from '../../assets/img/LogoFlexi.png';
+import LogoTradi from '../../assets/img/LogoTradi.png';
+import LogoPotion from '../../assets/img/LogoPotion.png';
+import LogoEtrange from '../../assets/img/LogoEtrange.png';
+import LogoCart from '../../assets/img/LogoCart.png';
 import Logo from '../Logo.jsx'
 
 
 function MenuFilters() {
     return (
-        <div className='Menufilters'>
-          
-            <div className='logo-menu'>
+        <div className='MenuFilters'>
+
+            <div className='logo-title-cart'>
                 <div className='logo-hpeat'>
                     <Logo />
                 </div>
+                    
+                    <h1>Bienvenue à HPEats !</h1>
+                    
+
+                <div className='logo-cart'>
+                <CardIcon logo={LogoCart} title = "Panier"/>
+                </div>
+            </div>
+            <div className='logo-menu'>
                 <CardIcon logo={LogoVegan} title = "Vegan"/>
                 <CardIcon logo={Logoflexi} title = "Flexi"/>
                 <CardIcon logo={LogoTradi} title = "Tradi"/>
                 <CardIcon logo={LogoPotion} title = "Potion"/>
                 <CardIcon logo={LogoEtrange} title = "Etrange"/>
             </div>
+            
         </div>
     )
 }
