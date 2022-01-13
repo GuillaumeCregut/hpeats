5ce6ef6131627696d6d50e1ebe30f0dcf286f3c8
Maud <mamaud2003@yahoo.fr>
2022-01-13 16:59:49 +0100

    Main space review

--- src/components/home-page/Main.jsx
+++ src/components/home-page/Main.jsx
@@ -1,21 +1,15 @@
 import React from 'react'
 import './Main.css'
 
-
 const Main = ({meal}) => {
     
     return (
         <div className='Main'>
-         <div className='main-container'>
-
+            <div className='main-container'>
                 <img className = 'mainImg' src={meal.picture} alt={meal.name} /> 
             </div>
-            <div>
-                
-
-         </div>
       </div>
     )
-
 }
+
 export default Main
