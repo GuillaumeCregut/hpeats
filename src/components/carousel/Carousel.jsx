659097055096a6db50621ed7baabc6cd5f98b4c5
Iysha Barton <iysha.barton@gmail.com>
2022-01-19 11:21:33 +0100

    Carousel has been modified

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -3,40 +3,39 @@ import InfiniteCarousel from "react-leaf-carousel";
 import AnimalCard from './AnimalCard';
 
 export default function Carousel({animalCard, totalWeight, updatePrice}) {
-  console.log(animalCard)
   return (
     <div>
-    <InfiniteCarousel
-    breakpoints={[
-      {
-        breakpoint: 500,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-        },
-      },
-      {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-        },
-      },
-    ]}
-    dots={true}
-    showSides={true}
-    sidesOpacity={0.5}
-    sideSize={0.1}
-    slidesToScroll={4}
-    slidesToShow={4}
-    scrollOnDevice={true}
-  >
-    {animalCard
-    .filter((element) => parseInt(element.payload) > totalWeight)
-    .map((element, index) => (
-  <AnimalCard key={index} src={element.image} alt={element.name} updatePrice={updatePrice} animalPrice={element.price} />
-    ))}
-    </InfiniteCarousel>
+      <InfiniteCarousel
+        breakpoints={[
+          {
+            breakpoint: 500,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2,
+            },
+          },
+          {
+            breakpoint: 768,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+            },
+          },
+        ]}
+        dots={true}
+        showSides={true}
+        sidesOpacity={0.5}
+        sideSize={0.1}
+        slidesToScroll={4}
+        slidesToShow={4}
+        scrollOnDevice={true}
+      >
+      {animalCard
+      .filter((element) => parseInt(element.payload) > totalWeight)
+      .map((element, index) => (
+    <AnimalCard key={index} src={element.image} alt={element.name} updatePrice={updatePrice} animalPrice={element.price} />
+      ))}
+      </InfiniteCarousel>
     </div>
   )
 }
