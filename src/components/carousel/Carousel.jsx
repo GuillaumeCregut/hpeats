bcd3408159c0069edf575ec3dd1317c0f87011f1
Guillaume Crégut <guillaume-cregut_student2021@wilder.school>
2022-01-20 12:00:56 +0100

    update carrousel

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -1,32 +1,17 @@
 import React from 'react';
 import Carousel from 'react-elastic-carousel'
 import AnimalCard from './AnimalCard';
-import axios from 'axios';
 
-export default function Carousel({ totalWeight, updatePrice }) {
-  const [animalCards, setAnimalCards] = React.useState([]);
-  const url = 'https://a.nacapi.com/HPEatsAnimals';
-
-  React.useEffect(() => {
-    axios
-      .get(url)
-      .then((res) => res.data)
-      .then((data) => setAnimalCards(data))
-  }, []);
-
-  export default function Slider({ animalCard, totalWeight, updatePrice }) {
-    return (
-      <div>
-        <Carousel itemsToShow={3}>
-          {animalCard
-            .filter((element) => parseInt(element.payload) > totalWeight)
-            .map((element, index) => (
-              <AnimalCard key={index} src={element.image} alt={element.name} updatePrice={updatePrice} animalPrice={element.price} />
-            ))}
-        </Carousel>
-      </div>
-    )
-  }
-
-
-
+export default function Slider({ animalCard, totalWeight, updatePrice }) {
+  return (
+    <div>
+      <Carousel itemsToShow={3}>
+        {animalCard
+          .filter((element) => parseInt(element.payload) > totalWeight)
+          .map((element, index) => (
+            <AnimalCard key={index} src={element.image} alt={element.name} updatePrice={updatePrice} animalPrice={element.price} />
+          ))}
+      </Carousel>
+    </div>
+  )
+}
