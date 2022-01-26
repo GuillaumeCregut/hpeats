9a2f70185fcd31290a679203e92572ee0f1c7bc1
Guillaume Crégut <guillaume-cregut_student2021@wilder.school>
2022-01-26 19:10:42 +0100

    add popup step 3

--- src/components/meals/MealCards.jsx
+++ src/components/meals/MealCards.jsx
@@ -1,12 +1,17 @@
 import { Link } from 'react-router-dom';
 import './MealCard.css';
 
-const MealCards = ({ mealCard, handleClick }) => {
-
+const MealCards = ({ mealCard, toggle }) => {
+    const handleClick = () => {
+        toggle(mealCard.id);
+    }
     return (
         <div className='MealCards'>
             <div className='meal-container'>
-                <img className="mealImage" src={mealCard.picture} alt={mealCard.name} onClick={handleClick(mealCard.id)} />
+                {/* <Link to={{ pathname: `/mealInfos/${mealCard.id}` }}>
+                    <img className="mealImage" src={mealCard.picture} alt={mealCard.name} />
+                </Link> */}
+                <img className="mealImage" src={mealCard.picture} alt={mealCard.name} onClick={handleClick} />
                 <h2>{mealCard.name}</h2>
                 <h3><span>Tarif:  </span>{mealCard.price}<i className="fab fa-d-and-d fa-1x MoneyIcon"></i></h3>
             </div>
@@ -15,3 +20,4 @@ const MealCards = ({ mealCard, handleClick }) => {
 }
 
 export default MealCards
+
