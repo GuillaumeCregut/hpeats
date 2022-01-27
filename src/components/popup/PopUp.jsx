21fc204cde2e2d1fa64f9d13b19d0856effbe15e
Guillaume Crégut <guillaume-cregut_student2021@wilder.school>
2022-01-27 11:38:24 +0100

    resolving css problems

--- src/components/popup/PopUp.jsx
+++ src/components/popup/PopUp.jsx
@@ -3,19 +3,19 @@ import '../meals/MealCardsLists.css';
 import './PopUp.css';
 const PopUp = ({ item2, addToCart, toggleVisibility }) => {
     return (
-        <div id={item2.id} className='MealHidden PopUpMail'>
+        <div id={item2.id} className='MealHidden PopUpMeal'>
             <p className='CloseCross'><span className='cross' onClick={() => toggleVisibility(item2.id)} >&times;</span></p>
             <h2 className='PopUpTitle'>{item2.name}</h2>
             <div className="PopUpContItem">
                 <img className='PopUpImage' src={item2.picture} alt={item2.name} />
                 <div>
                     <p className='PopUpDesc'>{item2.description}</p>
-                    <p className='PopUpPrice'>Prix : {item2.price}<i className="fab fa-d-and-d fa-1x MoneyIcon"></i></p>
+                    <p className='PopUpPrice'>Prix : {item2.price} <i className="fab fa-d-and-d fa-1x PopupMoneyIcon"></i></p>
                 </div>
             </div>
 
             <div className='PopUpBtn'>
-                <button className='mealCart' onClick={() => addToCart(item2)}>Ajouter au panier</button>
+                <button className='PopUpAddBtn' onClick={() => addToCart(item2)}>Ajouter au panier</button>
             </div>
         </div>
     )
