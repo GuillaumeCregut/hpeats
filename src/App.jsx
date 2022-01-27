87ff8be72aebb81bf89194e39eb81523eaf2a793
Jérémie Piazza <jeremiepiazza@sfr.fr>
2022-01-27 14:09:38 +0100

    add form

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,9 @@ import { useState } from "react";
 import Cart from './pages/Cart';
 import Home from './pages/Home';
 import UserPage from './pages/UserPage';
+import ContactPage from './pages/ContactPage';
 import './App.css';
+import LogoFormulaireDeContact from './assets/img/LogoFormulaireDeContact.png';
 import MealDetails from "./components/meals/MealDetails";
 
 
@@ -71,6 +73,7 @@ function App() {
           defaultUser={initialUser} />}
         />
         <Route path="mealInfos/:id" element={<MealDetails addToCart={addToCart} />} />
+        <Route path="/contact" element={<ContactPage logo={LogoFormulaireDeContact} title="FormulaireDeContact" />} />
       </Routes>
       
       
