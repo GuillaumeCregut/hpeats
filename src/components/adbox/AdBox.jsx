8292f92e187c3c5a14a5f99634a69fa71f35a873
Guillaume Crégut <guillaume-cregut_student2021@wilder.school>
2022-01-27 13:45:26 +0100

    adding adbox

--- src/components/adbox/AdBox.jsx
+++ src/components/adbox/AdBox.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
-
+import poulet from '../../assets/img/poulet.png';
+import stop from '../../assets/img/stop.png';
+import './AdBox.css';
 const AdBox = (props) => {
     return (
-        <div>
-
+        <div className='AdBox'>
+            <div className="AdBoxImgContainer">
+                <img src={poulet} alt='chicken' className='AdBoxImg Chicken' />
+                <img src={stop} alt='stop' className='AdBoxImg Stop' />
+            </div>
+            <div className="AdBoxSloganContainer">
+                <p className='AdSlogan'>Aucun poulet notable ? Normal, nous sommes une entreprise responsable</p>
+            </div>
         </div>
     )
 }
