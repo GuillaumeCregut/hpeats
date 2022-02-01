2672ac925838a8d63e53f7c2d86cdb6702557cdb
Guillaume Crégut <guillaume-cregut_student2021@wilder.school>
2022-02-01 15:43:30 +0100

    correcting mealcard

--- src/components/meals/MealCards.jsx
+++ src/components/meals/MealCards.jsx
@@ -8,7 +8,6 @@ const MealCards = ({ mealCard, toggle }) => {
         <div className='MealCards'>
             <div className='meal-container'>
                 <img className="mealImage" src={mealCard.picture} alt={mealCard.name} onClick={handleClick} />
-                <h2>{mealCard.name}</h2>
                 <div className='meal-desc'>
                     <h2>{mealCard.name} : {mealCard.price}<i className="fab fa-d-and-d fa-1x MoneyIcon"></i></h2>
                 </div>
